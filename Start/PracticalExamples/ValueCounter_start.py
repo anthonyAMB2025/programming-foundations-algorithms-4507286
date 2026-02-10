e2c31c1f6740cbd179f0bdc502e6d08766e3946f
anthonyAMB2025 <anthony@acceleratedmfgbrokers.com>
2026-02-10 21:10:18 +0000

    dictionary

--- Start/PracticalExamples/ValueCounter_start.py
+++ Start/PracticalExamples/ValueCounter_start.py
@@ -1,6 +1,6 @@
 # Example file for Programming Foundations: Algorithms by Joe Marini
 # using a hashtable to count individual items
-
+from collections import defaultdict
 
 # define a set of items that we want to count
 items = ["apple", "pear", "orange", "banana", "apple",
@@ -8,9 +8,12 @@ items = ["apple", "pear", "orange", "banana", "apple",
          "apple", "kiwi", "pear", "apple", "orange"]
 
 # TODO: create a hashtable object to hold the items and counts
-counter = None
+counter = defaultdict(int)
 
 # TODO: iterate over each item and increment the count for each one
+for i in items:
+  counter[i] += 1
+
 
 
 # print the results
